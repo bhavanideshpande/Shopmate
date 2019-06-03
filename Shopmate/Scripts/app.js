337e79319a1f739b9720a6f146e40dae53565902
Bhavani Deshpande <bhavani.deshpande28@gmail.com>
2019-06-03 20:23:00 +0530

    added angular js and fetched categories

---
+++ Shopmate/Scripts/app.js
@@ -0,0 +1,7 @@
+﻿var app = angular.module('categories', []);
+app.controller('catctrl', function ($scope, $http) {
+    $http.get("https://backendapi.turing.com/categories")
+        .then(function (response) {
+            $scope.data = response.data.rows;
+        });
+});
